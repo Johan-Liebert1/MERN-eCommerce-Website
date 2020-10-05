5195d8686155c25834deb44724544facef0692a4
Kenzo-Tenma-Test <pragya.41999@hotmail.com>
2020-10-05 18:19:23 +0530

    Redux

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
-import products from './data/products.js' // files need to suffixed with .js
-import connectDB from './config/db.js'
+import connectDB from './config/db.js'// files need to suffixed with .js
+
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -14,14 +16,12 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const p = products.find(p1 => p1._id === req.params.id)
-    res.json(p)
-})
+app.use(notFound)
+
+// custom error middleware now moved to ./middleware/errorMiddleware.js
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
