8f104570350912adffb7865d6d7e8acad1b9a603
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2020-10-07 12:59:47 +0530

    JWT Authorization

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler' // custom error handler
 import User from '../models/userModel.js'
-
+import generateToken from '../utils/generateToken.js'
 
 // Authenticate the user and get token
 
@@ -17,7 +17,7 @@ const authUser = asyncHandler( async (req, res) => {
             name : user.name,
             email : user.email,
             isAdmin : user.isAdmin,
-            token : null
+            token : generateToken(user._id)
         })
     }
 
@@ -27,4 +27,26 @@ const authUser = asyncHandler( async (req, res) => {
     }
 })
 
-export { authUser }
+
+const getUserProfile = asyncHandler( async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+
+        res.json({
+            _id : user._id,
+            name : user.name,
+            email : user.email,
+            isAdmin : user.isAdmin,
+        })
+    }
+
+    else {
+        res.status(404)
+        throw new Error("User not found")
+    }
+})
+
+
+
+export { authUser, getUserProfile }
