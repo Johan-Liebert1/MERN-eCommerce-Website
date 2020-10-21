9f8487c9bd5481ee375f1f9486e9757d38a1ff1d
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2020-10-21 20:49:26 +0530

    new

---
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { useDispatch, useSelector } from 'react-redux'
+
+const PlaceOrderScreen = () => {
+    const dispatch = useDispatch()
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PlaceOrderScreen
